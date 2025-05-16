ef44e9884b219d02178b9da554b67ffe188ab94c
hafsa talbi <hafsatllb@gmail.com>
2025-05-16 17:48:31 +0100

    first

---
+++ src/main/java/org/example/freelynk/FreelynkApplication.java
@@ -0,0 +1,13 @@
+package org.example.freelynk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelynkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreelynkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/freelynk/FreelynkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.freelynk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelynkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
